f315db20140eeae802684d7fe5398ee727edca3c
fylis <filip.siliwoniuk@gmail.com>
2025-11-28 14:01:31 +0100

    Adding a test FastMCP Server and Client that works locally and can be accessed by the port 8000

---
+++ tutorials/mcp/client.py
@@ -0,0 +1,15 @@
+"""
+Source : https://gofastmcp.com/getting-started/quickstart
+"""
+
+import asyncio
+from fastmcp import Client
+
+client = Client("http://localhost:8000/mcp")
+
+async def call_tool(name: str):
+    async with client:
+        result = await client.call_tool("greet", {"name": name})
+        print(result)
+
+asyncio.run(call_tool("Ford"))
