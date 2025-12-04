ca17b9fb2294db538b5955fdf58799bd7f82299e
fylis <filip.siliwoniuk@gmail.com>
2025-12-04 15:06:53 +0100

    Adding second LLM informations if needed to launch two Llama.cpp servers.

--- config.py
+++ config.py
@@ -5,13 +5,17 @@ Configuration settings for HAIWPA Chat Application.
 import os
 
 # Configuration for HAIWPA Chat Application
-SERVER_URL = "http://localhost:8000"
+SERVER_URL_1 = "http://localhost:8000"
+SERVER_URL_2 = "http://localhost:8001"
 API_KEY = "haiwpa-key"
 
 # Model configuration
-MODEL_PATH = "models/Llama-3.2-3B-Instruct-Q5_K_M.gguf"
-MODEL_ALIAS = "Llama 3.2 3B Instruct"
-TEMPERATURE = 0.7  # Less randomness, more predictable
+MODEL_PATH_1 = "models/Llama-3.2-3B-Instruct-Q5_K_M.gguf"
+MODEL_ALIAS_1 = "Llama 3.2 3B Instruct"
+MODEL_PATH_2 = "models/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf"
+MODEL_ALIAS_2 = "Llama 3.1 8B Instruct"
+TEMPERATURE_1 = 0.7  # Less randomness, more predictable
+TEMPERATURE_2 = 0.3
 MAX_TOKEN = 512
 
 # Gradio interface settings
@@ -19,4 +23,5 @@ GRADIO_SERVER_URL = "127.0.0.1"
 GRADIO_SERVER_PORT = 7860
 
 # JSON extraction context file
+DATA_FOLDER = "data"
 CONTEXT_FILE = "data/context.json"
