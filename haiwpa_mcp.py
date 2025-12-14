a4987c537e9fd774fe42b9cbc69233f1c20710f7
fylis <filip.siliwoniuk@gmail.com>
2025-12-15 00:36:58 +0100

    Updating of the script with :
    - `format_suggested_workout` : converts the list from Prolog to a single string containing alternative muscle groups separated by comma
    - Adding the `entry-type` which is used to difference past/future workout
    - Suggestion are available and working in the `validate_single_workout`
    - `validate_all_planned_workouts` : Send all planned muscle group to Prolog to tell if those can be worked out or not.

--- haiwpa_mcp.py
+++ haiwpa_mcp.py
@@ -26,15 +26,34 @@ prolog = Prolog()
 prolog.consult("workout_rules.pl")
 
 # Unit test to check if the connexion worked
-list(prolog.query("connexion_test."))
+list(prolog.query("connection_test."))
 
 
 def convert_date_to_timestamp(date_str: str):
     dt = datetime.strptime(date_str, "%Y-%m-%d")
-    return int(dt.timestamp())
+    return int(dt.timestamp())  # Convert to UNIX timestamp
 
 
-def load_data_from_json(file_path=config.CONTEXT_FILE):
+# Used to have a formatted string from the muscle group alternative list
+def format_suggested_workout(suggested_workout):
+    res = ""
+    for r in suggested_workout:
+        res += r["AlternativeMuscle"] + ", "
+    res = res[:-2]
+    return res
+
+
+def suggest_workout(muscle: str, date: str):
+    suggested_workout = list(
+        prolog.query(
+            f"suggest_alternative({muscle}, {convert_date_to_timestamp(date)}, AlternativeMuscle)."
+        )
+    )
+    res = format_suggested_workout(suggested_workout)
+    return res
+
+
+def load_json_workout_context(file_path=config.CONTEXT_FILE):
     # Check if file exists
     if not os.path.exists(file_path):
         return
@@ -42,6 +61,8 @@ def load_data_from_json(file_path=config.CONTEXT_FILE):
     # Clearing previous data in SWI-Prolog
     list(prolog.query("retractall(workout_history(_, _, _, _))."))
     list(prolog.query("retractall(injury(_, _))."))
+
+    planned_workout = []
 
     # Load JSON data
     with open(file_path, "r") as f:
@@ -53,25 +74,44 @@ def load_data_from_json(file_path=config.CONTEXT_FILE):
         exercises = entry["exercises"]
         duration = entry["duration"]
         injuries = entry["injuries"]
+        entry_type = entry["entry_type"]
 
         # Workout history assertion
-        if date and muscle:
+
+        if not date or not muscle:
+            continue
+
+        if entry_type == "completed":
             query = f"assertz(workout_history({convert_date_to_timestamp(date)}, '{muscle}', '{exercises}', {duration}))"
             list(prolog.query(query))
 
-        # Injuries assertion
-        if injuries and injuries.strip():
-            query = f"assertz(injury({convert_date_to_timestamp(date)}, '{muscle}'))"
-            list(prolog.query(query))
+            # Injuries assertion
+            if injuries and injuries.strip():
+                query = (
+                    f"assertz(injury({convert_date_to_timestamp(date)}, '{muscle}'))"
+                )
+                list(prolog.query(query))
+
+        elif entry_type == "planned":
+            planned_workout.append(
+                {
+                    "date": date,
+                    "muscle": muscle,
+                    "exercises": exercises,
+                    "duration": duration,
+                    "injuries": injuries,
+                    "entry_type": entry_type,
+                }
+            )
+
+    return planned_workout
 
 
-def validate_workout(muscle: str, date: str):
+def validate_single_workout(muscle: str, date: str):
     """
     Validate if a workout for a specific muscle group is allowed on a given date (yes/no).
     Returns {"approved": bool, "reason": str}
     """
-    load_data_from_json()
-
     # All atoms/muscles groups, etc. are in lowercase in SWI-Prolog
     muscle = muscle.lower()
 
@@ -84,21 +124,54 @@ def validate_workout(muscle: str, date: str):
 
     if results:
         reason = results[0]["Reason"]
-        # print(f"Prolog result : {reason}")
+        print(f"Prolog result : {reason}")
 
         if reason == "workout_allowed":
             return {"approved": True, "reason": "Approved for the muscle group."}
         elif reason == "injury_present":
-            return {"approved": False, "reason": "An injury is present."}
+            print("test 1")
+            suggested_workout_res = suggest_workout(muscle, date)
+            return {
+                "approved": False,
+                "reason": "An injury is present. Suggested alternatives : "
+                + suggested_workout_res,
+            }
         elif reason == "insufficient_rest":
-            return {"approved": False, "reason": "Insufficient rest on the muscle group."}
+            suggested_workout_res = suggest_workout(muscle, date)
+            return {
+                "approved": False,
+                "reason": "Insufficient rest on the muscle group. Suggested alternatives : "
+                + suggested_workout_res,
+            }
 
     # Check if workout is allowed
     return {"approved": False, "reason": "Unknown reason"}
 
 
-load_data_from_json()
-print(validate_workout("biceps", "2025-12-09"))
+@mcp.tool()
+def validate_all_planned_workouts():
+    planned_workouts = load_json_workout_context()
+    results = []
 
-# if __name__ == "__main__":
-#     mcp.run()
+    if not planned_workouts:
+        return results
+
+    for workout in planned_workouts:
+        validation = validate_single_workout(workout["muscle"], workout["date"])
+        results.append(
+            {
+                "date": workout["date"],
+                "muscle": workout["muscle"],
+                "exercises": workout["exercises"],
+                "duration": workout["duration"],
+                "injuries": workout["injuries"],
+                "entry_type": workout["entry_type"],
+                "validation": validation,
+            }
+        )
+
+    return results
+
+
+if __name__ == "__main__":
+    mcp.run()
